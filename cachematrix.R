d442c76f41e7208a3f2e3ae36f6d12a4d864eb1e
Saurabh Rajguru <saurabhrajguru7@gmail.com>
2018-05-27 20:27:18 +0530

    Inverse of a Matrix
    The inverse is taken from the cache if it has already been calculated otherwise it is calculated and then put in the cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions compute the inverse of a matrix depending on 
+## whether it has already been calculated or not.
 
-## Write a short comment describing this function
+## makeCacheMatrix function creates a new object which has functions 
+## set,get,getinv,setinv and values-x,m
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes as argument the new object created by 
+## makeCacheMatrix. It finds the iverse of the argument depending
+## on whether it has already been calculated or not.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setinv(m)
+  m       ## Return a matrix that is the inverse of 'x'
 }
